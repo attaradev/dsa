94908051d6da34c2035500b91f543d639a98cb93
Mike Attara <mpyebattara@gmail.com>
2023-03-14 09:19:36 +0000

    matrix shortest path

--- src/main/java/dev/attara/arrays/Matrix.java
+++ src/main/java/dev/attara/arrays/Matrix.java
@@ -2,14 +2,14 @@ package dev.attara.arrays;
 
 public class Matrix {
     public static int shortestPath(int[][] matrix){
-        int sum = 0, i = 0, j = 0;
+        int path = 0, i = 0, j = 0;
         while (i < matrix.length && j < matrix[i].length) {
-            sum += matrix[i][j];
+            path += matrix[i][j];
             int r = (j+1) < matrix[i].length ? matrix[i][j+1] : Integer.MAX_VALUE;
             int d = (i+1) < matrix.length ? matrix[i+1][j] : Integer.MAX_VALUE;
             if (r < d) j++;
             else i++;
         }
-        return sum;
+        return path;
     }
 }
