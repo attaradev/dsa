9bb82332daad849bd2ae56dbe2dedc45e20e6016
Mike Attara <mpyebattara@gmail.com>
2023-03-15 09:03:41 +0000

    find largest submatrix

--- src/main/java/dev/attara/arrays/Matrix.java
+++ src/main/java/dev/attara/arrays/Matrix.java
@@ -1,29 +1,72 @@
 package dev.attara.arrays;
 
-//  Given an nxn-dimensional matrix of integers, find the shortest path from the top left-hand corner to the bottom right-hand corner.
-//  You can move horizontally and vertically, and always forward (i.e. from left to right, or top to bottom).
-//  The shortest path is the smallest sum of all values you encounter on the way.
-//
-//  As an example:
-//  For the matrix
-//  6 3 5
-//  2 4 1
-//  7 2 9
-//  the shortest path is 22,
-//  6
-//  2 4 1
-//      9
-
 public class Matrix {
+    //  Given an nxn-dimensional matrix of integers, find the shortest path from the top left-hand corner to the bottom right-hand corner.
+    //  You can move horizontally and vertically, and always forward (i.e. from left to right, or top to bottom).
+    //  The shortest path is the smallest sum of all values you encounter on the way.
+    //
+    //  As an example:
+    //  For the matrix
+    //  6 3 5
+    //  2 4 1
+    //  7 2 9
+    //  the shortest path is 22,
+    //  6
+    //  2 4 1
+    //      9
     public static int shortestPath(int[][] matrix){
-        int path = 0, i = 0, j = 0;
-        while (i < matrix.length && j < matrix[i].length) {
-            path += matrix[i][j];
-            int right = (j+1) < matrix[i].length ? matrix[i][j+1] : Integer.MAX_VALUE;
-            int bottom = (i+1) < matrix.length ? matrix[i+1][j] : Integer.MAX_VALUE;
-            if (right < bottom) j++;
-            else i++;
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        dp[0][0] = matrix[0][0];
+
+        // initialize first row
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j-1] + matrix[0][j];
         }
-        return path;
+
+        // initialize first column
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i-1][0] + matrix[i][0];
+        }
+
+        // fill in the rest of the matrix
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n-1][n-1];
+    }
+
+    // Given an M × N binary matrix, find the size of the largest square submatrix of 1's present.
+    public static int largestSquareSubmatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int maxSize = 0;
+
+        // initialize first row and column of dp
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = matrix[i][0];
+            maxSize = Math.max(maxSize, dp[i][0]);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j];
+            maxSize = Math.max(maxSize, dp[0][j]);
+        }
+
+        // fill in the rest of dp
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                    maxSize = Math.max(maxSize, dp[i][j]);
+                }
+            }
+        }
+
+        return maxSize * maxSize; // return area of the largest square submatrix
     }
 }
--- src/test/java/dev/attara/arrays/MatrixTest.java
+++ src/test/java/dev/attara/arrays/MatrixTest.java
@@ -7,7 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class MatrixTest {
     @Test
     void shortestPath() {
-        int[][] matrix = {{6, 3, 5}, {2, 4, 1}, {7, 2, 9}};
-        assertEquals(22,Matrix.shortestPath(matrix));
+        int[][] matrix1 = {{6, 3, 5}, {2, 4, 1}, {7, 2, 9}};
+        int[][] matrix2 = {{6, 1, 1}, {0, 4, 1}, {0, 4, 2}};
+        int[][] matrix3 = {{6, 3, 1}, {0, 4, 1}, {0, 4, 2}};
+        assertEquals(22,Matrix.shortestPath(matrix1));
+        assertEquals(11, Matrix.shortestPath(matrix2));
+        assertEquals(12, Matrix.shortestPath(matrix3));
+    }
+
+    @Test
+    void largestSquareSubmatrix(){
+        int[][] matrix = {
+                {1, 0, 1, 0, 0},
+                {1, 0, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 0, 0, 1, 0}};
+        assertEquals(4, Matrix.largestSquareSubmatrix(matrix));
     }
 }
